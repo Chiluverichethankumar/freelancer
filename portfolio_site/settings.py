9c494f5f55db3ed284839e280ef0cde627fbf553
ChiluverchethanKumar <chiluverichethankumar@gmail.com>
2025-08-12 03:43:38 +0530

    1\j

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '127.0.0.1', 'freelancer.up.railway.app']
+ALLOWED_HOSTS = ['freelancer.up.railway.app', 'localhost', '127.0.0.1']
 
 # Application definition
 
