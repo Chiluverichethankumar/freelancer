de5e1d86b76fe41cec61e4dc23862525187e6a00
ChiluverchethanKumar <chiluverichethankumar@gmail.com>
2025-08-12 14:04:16 +0530

    Django project ready for Railway

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -21,13 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: ALLOWED_HOSTS  the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['freelancer.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'freelancer.up.railway.app',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
